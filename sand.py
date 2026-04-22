e475555bd90d48309926198ce375c1bbc587441e
William Ni <niwilliam741@gmail.com>
2026-04-22 18:45:25 -0400

    filereader & other files

---
+++ sand.py
@@ -0,0 +1,27 @@
+
+# File reader
+def readNCAP(txt_path):
+    icmp_lines = []
+
+    with open(txt_path, "r", errors="ignore") as file:
+        for line in file:
+            stripped = line.strip()
+
+            # All summary lines start with a ID; if a line 
+            # does not start with a ID or is a blank line, 
+            # it will be skipped
+            if not stripped or not stripped[0].isdigit():
+                continue
+
+            # Drops any summary line that isn't ICMP
+            if " ICMP " not in f" {line} ":
+                continue
+
+            # Drop destination unreachable ICMP packets
+            # This also drops all hexcodes
+            if "Destination unreachable" in line:
+                continue
+
+            icmp_lines.append(line.rstrip("\n"))
+
+    return icmp_lines
