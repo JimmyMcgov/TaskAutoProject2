9495e1f8ecd0dca7a487a7259bcf954e2ea46894
William Ni <niwilliam741@gmail.com>
2026-04-22 18:57:18 -0400

    main setup

--- main.py
+++ main.py
@@ -8,11 +8,31 @@ import compute_metrics as compute
 
 
 def main():
-    pass
+    filepaths = [
+        "Captures/Node1.txt",
+        "Captures/Node2.txt",
+        "Captures/Node3.txt",
+        "Captures/Node4.txt"
+        ]
+    
+    for nodePath in filepaths:
+        validPackets = filter.readNCAP(nodePath)
 
+        # packetDB = final mashmap of packet parser
+        packetDB = parser.PLACEHOLD(validPackets)
 
+        computedList = compute.PLACEHOLD(packetDB)
+        
+        print(f"PLACEHOLD METRIC: {computedList[0]}")
+        print(f"PLACEHOLD METRIC: {computedList[1]}")
+        print(f"PLACEHOLD METRIC: {computedList[2]}")
+        print(f"PLACEHOLD METRIC: {computedList[3]}")
+        print(f"PLACEHOLD METRIC: {computedList[4]}")
+        print(f"PLACEHOLD METRIC: {computedList[5]}")
+        # ...
 
-
+        # Seperator between each summary
+        print()
 
 
 
