8d8c351c3e099b0fec8304dea191cdcd91a8d3ca
William Ni <niwilliam741@gmail.com>
2026-04-25 17:22:28 -0400

    comments & extra cleanup

--- compute_metrics.py
+++ compute_metrics.py
@@ -3,37 +3,39 @@ import csv
 from pathlib import Path
 import sys
 
-def calculate_metrics(parsed_file, node_id=1):
-    #Dictionaries used to track requests for RTT/Matching
-    #key is (id, seq)
+def calculate_metrics(parsed_file, node_id = 1):
+
+    # Dictionaries used to track 
+    # requests for RTT/Matching
+    # Keys are (id, seq)
     requests = {}
     recieved_requests = {}
     
-    #accumulators
+    # Accumulators
     stats = {
-            'req_sent': 0, 'req_recieved': 0,
-            'rep_sent': 0, 'rep_recieved': 0,
-            'bytes_req_sent': 0, 'bytes_req_recv': 0,
-            'payload_req_sent': 0, 'payload_req_recv': 0,
-            'total_rtt': 0, 'total_hops': 0, 'matched_pairs': 0,
-            'total_reply_delay': 0, 'delay_pairs': 0
-            
+        'req_sent': 0, 'req_recieved': 0,
+        'rep_sent': 0, 'rep_recieved': 0,
+        'bytes_req_sent': 0, 'bytes_req_recv': 0,
+        'payload_req_sent': 0, 'payload_req_recv': 0,
+        'total_rtt': 0, 'total_hops': 0, 'matched_pairs': 0,
+        'total_reply_delay': 0, 'delay_pairs': 0
     }
 
     host_ip = None
 
-    HEADER_SIZE = 42 #ethernet(14) + IP(20) + ICMP(8)
+    # Ethernet(14) + IP(20) + ICMP(8)
+    HEADER_SIZE = 42 
     try:
         with open(parsed_file, mode="r") as f:
             reader = csv.DictReader(f)
             for row in reader:
 
-                # we are assuming first request is 
+                # We are assuming first request is 
                 # from the host
                 if host_ip is None:
                     host_ip = row['src']
 
-                # packet info
+                # Packet info
                 src = row['src']
                 dest = row['dst']
                 t = float(row['time'])
@@ -44,38 +46,55 @@ def calculate_metrics(parsed_file, node_id=1):
                 ttl = int(row['ttl'])
                 payload = b - HEADER_SIZE
 
+                # Begin by checking if a packet is
+                # a request
                 if p_type == 'request':
+
+                    # Within each request, a packet
+                    # can originate from the host
+                    # or another device
                     if src == host_ip:
                         stats['req_sent'] += 1
                         stats['bytes_req_sent'] += b
                         stats['payload_req_sent'] += payload
 
-                        # store timestamp for RTT calculation
+                        # Time stamps of packets that originate
+                        # from the host
                         requests[(p_id, p_seq)] = t
 
+                    # Packets that do not originate from the 
+                    # host must come from another device
                     elif dest == host_ip:
                         stats['req_recieved'] += 1
                         stats['bytes_req_recv'] += b
                         stats['payload_req_recv'] += payload
 
+                        # Time stamps of packets that originate
+                        # from other devices
                         recieved_requests[(p_id, p_seq)] = t
 
-
+                # If a packet isn't a request,
+                # it must be a reply
                 elif p_type == 'reply':
-                    
+
                     if src == host_ip:
                         stats['rep_sent'] += 1
 
+                        # Replies require a request to solicite the 
+                        # reply; thus we check if there is a recorded
+                        # request that corresponds to our reply
+                        # Delay
                         if (p_id, p_seq) in recieved_requests:
                             req_t = recieved_requests.pop((p_id, p_seq))
                             delay = t - req_t
                             stats['total_reply_delay'] += delay
                             stats['delay_pairs'] += 1
 
+
                     elif dest == host_ip:
                         stats['rep_recieved'] += 1
                         
-                        # match reply with original request
+                        # RTT
                         if(p_id, p_seq) in requests:
                             req_t = requests[(p_id, p_seq)]
                             rtt = t - req_t
@@ -83,28 +102,21 @@ def calculate_metrics(parsed_file, node_id=1):
                             stats['total_rtt'] += rtt
                             stats['matched_pairs'] += 1
 
-                        # hop count calculation using standard ttl baselines
-                        # linux, mac, IoT
+                        # Hop counts
+                        # Linux, Mac, IoT
                         if ttl <= 64: 
                             stats['total_hops'] += (64 - ttl) + 1
                         # Windows
                         elif ttl <= 128: 
                             stats['total_hops'] += (128 - ttl) + 1
-                        # network devices
+                        # Network devices
                         else: 
                             stats['total_hops'] += (255 - ttl) + 1
 
         # Derived averages
         avg_rtt_ms = (stats['total_rtt'] * 1000) / stats['matched_pairs'] if stats['matched_pairs'] > 0 else 0
-
         avg_delay_us = (stats['total_reply_delay'] * 1000000) / stats['delay_pairs'] if stats['delay_pairs'] > 0 else 0
         avg_hops = stats['total_hops'] / stats['matched_pairs'] if stats['matched_pairs'] > 0 else 0
-
-        # print(f"{node_id}")
-        # print(f"Total RTT: {stats['total_rtt']}")
-        # print(f"Matched Pairs: {stats['matched_pairs']}")
-        # print(f"Total hops: {stats['total_rtt']}")
-        # print()
 
         # Throughput/Goodput (sum of request bytes / sum of RTT)
         sum_rtt = stats['total_rtt']
@@ -133,25 +145,9 @@ def calculate_metrics(parsed_file, node_id=1):
         Path("computed/").mkdir(exist_ok = True)
         Path(file_path).touch()
 
-        # script_dir = os.path.dirname(__file__)
-        # file_path = os.path.join(script_dir, 'project_2_results.csv')
-        # fileExists = os.path.exists(f'computed/project_2_Node{node_id}_results.csv')
-
-
-        # script_dir = os.path.dirname(__file__)
-        # file_path = os.path.join(script_dir, 'project_2_results.csv')
-        # fileExists = os.path.exists('project_2_results.csv')
-
         with open(file_path, mode = 'a', newline = '') as csvfile:
             writer = csv.writer(csvfile)
-            # if not fileExists:
-            # if not Path(file_path).is_file():
-            #     writer.writerow(['Node', 'Category', 'Metric', 'Value'])
-            
             writer.writerows(output_rows)
 
     except Exception as e:
         print(f'Error processing file: {e}')
-
-if __name__ == "__main__":
-    calculate_metrics(sys.argv[1])
